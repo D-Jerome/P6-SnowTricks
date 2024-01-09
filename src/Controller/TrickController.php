1f4c998b0eaa34612425599cf362ce96f2f76d94
Jerome Dubus <dubus.jerome@gmail.com>
2024-01-09 14:49:18 +0100

    #2 Cs fixer verif

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,9 @@ class TrickController extends AbstractController
     #[Route('/trick/{slug<((\w+)-){0,}(\w+)>?}', name: 'app_trick')]
     public function showTrick(string $slug): Response
     {
-
         return $this->render('trick/index.html.twig', [
             'controller_name' => 'TrickController',
-            'slug' => $slug,
+            'slug'            => $slug,
         ]);
     }
 }
