04c6e648912ad1c84c318961524c225d62d645ea
Jerome Dubus <dubus.jerome@gmail.com>
2024-01-11 00:07:34 +0100

    View Tricks ( Home Page) View Trick ( selected Slug)

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class TrickController extends AbstractController
 {
     #[Route('/trick/{slug<((\w+)-){0,}(\w+)>?null}', name: 'app_trick')]
-    public function showTrick(string $slug): Response
+    public function showTrick(string $slug, TrickRepository $trick): Response
     {
         return $this->render('trick/index.html.twig', [
-            'controller_name' => 'TrickController',
-            'slug'            => $slug,
+            'controller_name'  => 'TrickController',
+            'trick'            => $trick->findOneBySlug($slug),
         ]);
     }
 }
