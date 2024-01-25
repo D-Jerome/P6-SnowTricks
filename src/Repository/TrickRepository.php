d23ec1ca3394b08011a12b33b8a53639233d6ba8
Jerome Dubus <dubus.jerome@gmail.com>
2024-01-25 16:46:02 +0100

    feat pagination - Home page => OK

--- src/Repository/TrickRepository.php
+++ src/Repository/TrickRepository.php
@@ -6,6 +6,7 @@ namespace App\Repository;
 
 use App\Entity\Trick;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -18,9 +19,23 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class TrickRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE = 15;
+    
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Trick::class);
+    }
+
+    public function getTrickPaginator(int $offset): Paginator
+    {
+        $query = $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery()
+        ;
+
+        return new Paginator($query);
     }
 
     //    /**
